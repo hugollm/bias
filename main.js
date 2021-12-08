077460a98a31eda2fa2e4d8a9442b942ecbb48e4
Hugo Mota <hugo.txt@gmail.com>
2021-12-08 15:24:14 -0300

    Refactor state

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 ReactDOM.render(<Main/>, document.getElementById('main'));
 
 function Main() {
-    let [criteria, setCriteria] = React.useState([
-        {name: 'Margin', score: 5},
-        {name: 'Dividends', score: 4},
-        {name: 'Expectation', score: 4},
-        {name: 'Safety', score: 4},
-    ]);
-    let [options, setOptions] = React.useState([
-        {name: 'B3SA3', scores: {'Margin': 2, 'Dividends': 3, 'Expectation': 5, 'Safety': 4}},
-        {name: 'BBAS3', scores: {'Margin': 5, 'Dividends': 4, 'Expectation': 4, 'Safety': 5}},
-        {name: 'WIZS3', scores: {'Margin': 4, 'Dividends': 5, 'Expectation': 4, 'Safety': 3}},
-        {name: 'JHSF3', scores: {'Margin': 5, 'Dividends': 5, 'Expectation': 5, 'Safety': 1}},
-        {name: 'ITSA4', scores: {'Margin': 4, 'Dividends': 5, 'Expectation': 4}},
-    ]);
+    let [state, setState] = React.useState({
+        criteria: {
+            'Margin': {name: 'Margin', weight: 5},
+            'Dividends': {name: 'Dividends', weight: 4},
+            'Expectation': {name: 'Expectation', weight: 4},
+            'Safety': {name: 'Safety', weight: 4},
+        },
+        options: {
+            'B3SA3': {name: 'B3SA3', scores: {'Margin': 2, 'Dividends': 3, 'Expectation': 5, 'Safety': 4}},
+            'BBAS3': {name: 'BBAS3', scores: {'Margin': 5, 'Dividends': 4, 'Expectation': 4, 'Safety': 5}},
+            'WIZS3': {name: 'WIZS3', scores: {'Margin': 4, 'Dividends': 5, 'Expectation': 4, 'Safety': 3}},
+            'JHSF3': {name: 'JHSF3', scores: {'Margin': 5, 'Dividends': 5, 'Expectation': 5, 'Safety': 1}},
+            'ITSA4': {name: 'ITSA4', scores: {'Margin': 4, 'Dividends': 5, 'Expectation': 4}},
+        },
+        ranking: ['B3SA3', 'BBAS3', 'WIZS3', 'JHSF3', 'ITSA4'],
+    });
     return <main>
-        <CritSection criteria={criteria} setCriteria={setCriteria}/>
-        <OptionSection options={options} setOptions={setOptions} criteria={criteria}/>
-        <RankedSection/>
+        <CritSection state={state} setState={setState}/>
+        <OptionSection state={state} setState={setState}/>
+        <RankedSection state={state}/>
     </main>
 }
 
-function CritSection({ criteria, setCriteria }) {
+function CritSection({ state, setState }) {
     return <section>
         <h2>Criteria</h2>
         <NewForm/>
-        <CritList criteria={criteria}/>
+        <CritList state={state} setState={setState}/>
     </section>
 }
 
@@ -36,67 +39,65 @@ function NewForm() {
     </form>
 }
 
-function CritList({ criteria }) {
+function CritList({ state, setState }) {
     return <ul>
-        {criteria.map(crit => <CritCard key={crit.name} crit={crit}/>)}
+        {Object.keys(state.criteria).map(name => <CritCard
+            key={name} crit={state.criteria[name]} setState={setState}
+        />)}
     </ul>
 }
 
-function CritCard({ crit }) {
+function CritCard({ crit, setState }) {
     return <li className="crit-card">
         <h3>{crit.name}</h3>
         <div>
             <label>Weight </label>
-            <input name={crit.name} type="radio" value={1} defaultChecked={(crit.score || 3) == 1}/>
-            <input name={crit.name} type="radio" value={2} defaultChecked={(crit.score || 3) == 2}/>
-            <input name={crit.name} type="radio" value={3} defaultChecked={(crit.score || 3) == 3}/>
-            <input name={crit.name} type="radio" value={4} defaultChecked={(crit.score || 3) == 4}/>
-            <input name={crit.name} type="radio" value={5} defaultChecked={(crit.score || 3) == 5}/>
+            {[1, 2, 3, 4, 5, 6, 7].map(n => {
+                return <input key={n} name={crit.name} type="radio" value={n}
+                    defaultChecked={(crit.weight || 5) == n}/>
+            })}
         </div>
     </li>
 }
 
-function OptionSection({ options, setOptions, criteria }) {
+function OptionSection({ state, setState }) {
     return <section>
         <h2>Options</h2>
         <NewForm/>
-        <OptionList options={options} criteria={criteria}/>
+        <OptionList state={state} setState={setState}/>
     </section>
 }
 
-function OptionList({ options, criteria }) {
+function OptionList({ state, setState }) {
     return <ul>
-        {options.map(option => <OptionCard key={option.name} option={option} criteria={criteria}/>)}
+        {Object.keys(state.options).map(name => <OptionCard
+            key={name} option={state.options[name]} state={state} setState={setState}
+        />)}
     </ul>
 }
 
-function OptionCard({ option, criteria }) {
+function OptionCard({ option, state, setState }) {
     return <li className="option-card">
         <h3>{option.name}</h3>
-        {criteria.map(crit => <div key={crit.name}>
-            <label>{crit.name} </label>
-            <input name={option.name + '|' + crit.name} type="radio" value={1} defaultChecked={(option.scores[crit.name] || 3) == 1}/>
-            <input name={option.name + '|' + crit.name} type="radio" value={2} defaultChecked={(option.scores[crit.name] || 3) == 2}/>
-            <input name={option.name + '|' + crit.name} type="radio" value={3} defaultChecked={(option.scores[crit.name] || 3) == 3}/>
-            <input name={option.name + '|' + crit.name} type="radio" value={4} defaultChecked={(option.scores[crit.name] || 3) == 4}/>
-            <input name={option.name + '|' + crit.name} type="radio" value={5} defaultChecked={(option.scores[crit.name] || 3) == 5}/>
+        {Object.keys(state.criteria).map(critName => <div key={critName}>
+            <label>{critName} </label>
+            {[1, 2, 3, 4, 5, 6, 7].map(n => {
+                return <input key={n} name={option.name + '|' + critName} type="radio" value={n}
+                    defaultChecked={(option.scores[critName] || 5) == n}/>
+            })}
         </div>)}
     </li>
 }
 
-function RankedSection() {
+function RankedSection({ state }) {
     return <section>
         <h2>Ranking</h2>
-        <RankedList/>
+        <RankedList state={state}/>
     </section>
 }
 
-function RankedList() {
+function RankedList({ state }) {
     return <ol>
-        <li>FB</li>
-        <li>AMZN</li>
-        <li>BABA</li>
-        <li>AAPL</li>
-        <li>TSLA</li>
+        {state.ranking.map((name, i) => <li key={name}>{i+1}. {name}</li>)}
     </ol>
 }
